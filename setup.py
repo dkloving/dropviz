2c02c531cc4b228b5c2a76dde54fc1c0b1b61658
David <davidkloving@gmail.com>
2020-12-16 15:17:55 -0600

    Ok NOW version is updated. Sheesh.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='dropviz',
-      version='0.0.1',
+      version='0.0.2',
       description='Visualize dropout as data augmentation.',
       author='David Loving',
       author_email='user@email.com',
